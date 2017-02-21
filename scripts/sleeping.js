471e86b3b9d007e7c7f41903cb500c50b1a4019f
Juntao Qiu <juntao.qiu@gmail.com>
2017-02-21 13:42:33 +0800

    make the sleeping chart more fancy

--- scripts/sleeping.js
+++ scripts/sleeping.js
@@ -1,25 +1,3 @@
-// var parseDate = d3.time.format("%Y-%m-%d").parse;
-
-// d3.csv("data/sleeping_normolized.csv", function(e, data) {
-// 	data.forEach(function(d) {
-// 		d.date = parseDate(d.date);
-// 		d.count = +(+d.length/60).toFixed(2);
-// 	});
-
-// 	var chart = calendarHeatmap()
-//       .data(data)
-//       .selector('.container')
-//       .colorRange(['#D8E6E7', "#e76818"])
-//       .tooltipEnabled(true)
-//       .tooltipUnit('Sleeping hours')
-//       .startDate(new Date('2016-11-09'))
-//       .onClick(function (data) {
-//         console.log('onClick callback. Data:', data);
-//       });
-
-// 	chart();
-// });
-
 var margin = {
 	top: 20, left: 50, right: 20, bottom: 20
 };
@@ -28,12 +6,12 @@ var width = 800 - margin.left - margin.right;
 var height = 450 - margin.top - margin.bottom;
 
 // var xScale = d3.time.scale().range([0, width]);
-var xScale = d3.scale.ordinal().rangeRoundBands([0, width], .05)
+var xScale = d3.scale.ordinal().rangeRoundBands([0, width], .5)
 
 //linear 24 hour scale
 var yScale = d3.scale.linear()
-  .domain([24,0])
-  .range([height,0]);
+  .domain([24, 0])
+  .range([height, 0]);
 
 //vertical axis
 var yAxis = d3.svg.axis()
@@ -41,14 +19,10 @@ var yAxis = d3.svg.axis()
   .ticks(24)
   .scale(yScale)
 
-// var xAxis = d3.svg.axis().scale(xScale)
-// 	.orient("bottom")
-// 	.ticks(10);
-
 var xAxis = d3.svg.axis()
     .scale(xScale)
-    .orient("bottom");
-    // .tickFormat(d3.time.format("%Y/%m/%d %H:%M"));
+    .orient("bottom")
+    .tickFormat(d3.time.format("%Y/%m/%d"));
 
 var tickArray = yScale.ticks(24),
   tickDistance = yScale(tickArray[tickArray.length - 1]) - yScale(tickArray[tickArray.length - 2]);
@@ -63,6 +37,7 @@ var svg = d3.select(".container").append("svg")
 		"translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv('data/sleeping_data.csv', function(e, data) {
+	// data = _.take(data, 50);
 	data.forEach(function(d) {
 		d.date = parseDate(d.date);
 		d.length = d.length;
@@ -73,18 +48,25 @@ d3.csv('data/sleeping_data.csv', function(e, data) {
 		return d.date;
 	}));
 
-	xScale.domain(data.map(function(d) { return d.date; }));
+	var dateRange = _.uniq(data, function(d) {
+		var date = d.date;
+		return [date.getYear(), date.getMonth(), date.getDate()].join("/");
+	});
 
-	svg.append("g")
-	  .attr("class", "x axis")
-	  .attr("transform", "translate(0," + height + ")")
-	  .call(xAxis)
-	.selectAll("text")
-	  .style("text-anchor", "end")
-	  .attr("dx", "-.8em")
-	  .attr("dy", "-.55em")
-	  .attr("transform", "rotate(-45)");
+	xScale.domain(dateRange.map(function(d) { return d.date; }));
 
+
+	function getFirstInDomain(date) {
+		var domain = xScale.domain();
+
+		var index = _.findIndex(domain, function(d) {
+			return date.getYear() === d.getYear() 
+				&& date.getMonth() === d.getMonth() 
+				&& date.getDate() === d.getDate();
+		});
+
+		return domain[index];
+	}
 	//draw the axis
 	svg.append("g")
 		.attr("class", "y axis")
@@ -93,21 +75,64 @@ d3.csv('data/sleeping_data.csv', function(e, data) {
 		.selectAll("text")
 		.attr("transform", "translate(-25,0)")
 
+	svg.selectAll(".barbg")
+		.data(data)
+		.enter()
+		.append("rect")
+		.attr("class", "barbg")
+		.attr("x", function(d) {
+			var m = getFirstInDomain(d.date);
+			return xScale(m) + margin.left;
+		})
+		.attr("width", xScale.rangeBand())
+		.attr("y", function(d) { 
+		  return margin.top; 
+		})
+		.attr("height", function(d) {
+		  return height;
+		});
+
 	svg.selectAll(".bar")
-	.data(data)  
-	.enter()
-	.append("rect")
-	.attr("class", "bar")
-	.attr("x", function(d) {
-		return xScale(d.date)+margin.left;
-	})
-	.attr("width", xScale.rangeBand())
-	.attr("y", function(d) { 
-	  var start = d.date.getHours();
-	  return yScale(start) + margin.top; 
-	})
-	.attr("height", function(d) {
-	  var offset = (d.length / 60).toFixed(2);
-	  return offset * tickDistance;
-	});	  		
+		.data(data)  
+		.enter()
+		.append("rect")
+		.attr("class", function(d) {
+			if(d.length < 60) {
+				return "low bar";
+			} else if(d.length >= 60 && d.length < 120) {
+				return "fine bar";
+			} else if(d.length >= 120 && d.length < 180) {
+				return "medium bar";
+			} else if(d.length >= 180 && d.length < 240) {
+				return "good bar";
+			} else if(d.length >= 240 && d.length < 300) {
+				return "great bar";
+			} else {
+				return "prefect bar";
+			}
+		})
+		.attr("x", function(d) {
+			var m = getFirstInDomain(d.date);
+			return xScale(m) + margin.left;
+		})
+		.attr("width", xScale.rangeBand())
+		.attr("y", function(d) { 
+		  var start = d.date.getHours() + (+(d.date.getMinutes()/60).toFixed(4));
+		  return yScale(start) + margin.top; 
+		})
+		.attr("height", function(d) {
+		  var offset = (d.length / 60).toFixed(2);
+		  return offset * tickDistance;
+		});
+
+
+	svg.append("g")
+	  .attr("class", "x axis")
+	  .attr("transform", "translate(0," + (height + margin.top) + ")")
+	  .call(xAxis)
+	.selectAll("text")
+	  .style("text-anchor", "end")
+	  .attr("dx", "-.8em")
+	  .attr("dy", "-.55em")
+	  .attr("transform", "rotate(-45)");		
 });
